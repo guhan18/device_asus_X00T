e4d90715ae0954db62be7fa356c6ec0f1a8f0ac8
LuK1337 <priv.luk@gmail.com>
2020-05-16 09:51:07 +0530

    X00TD: Exclude Snap overlays from RRO
    Signed-off-by: Vivekachooz <vivekachooz@gmail.com>
    Change-Id: Ibefb7acaa158eea787618d1696057b75d2541e87

--- device.mk
+++ device.mk
@@ -346,7 +346,8 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
+    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
 
 # Power
 PRODUCT_PACKAGES += \
